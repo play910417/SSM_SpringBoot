014961bd8a8bb8d09d8d07b9b8ed27f41d1f6571
play910417 <609434120@qq.com>
2019-06-27 12:07:15 +0800

    ver 1.0

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,6 +1,5 @@
-var path = $("#path").val();
-var imgYes = "<img width='15px' src='"+path+"/images/y.png' />";
-var imgNo = "<img width='15px' src='"+path+"/images/n.png' />";
+var imgYes = "<img width='15px' src='/images/y.png' />";
+var imgNo = "<img width='15px' src='/images/n.png' />";
 /**
  * 提示信息显示
  * element:显示提示信息的元素（font）
